1e35b0fd7a258c35bc985e33aaa102696aa2420f
Jideofor Nwoke <njusticej@gmail.com>
2020-02-21 16:30:42 +0100

    add header component to app.js

--- project-management-tool-react/src/App.js
+++ project-management-tool-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import DashBoard from './components/DashBoard';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/layout/Header';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <DashBoard/>
     </div>
   );
