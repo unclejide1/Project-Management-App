809ef385ff68191b09945e1d90079472062f76ed
Jideofor Nwoke <njusticej@gmail.com>
2020-02-21 01:25:24 +0100

    add project Service

--- project-Manager/src/main/java/com/jide/ppmtool/services/ProjectService.java
+++ project-Manager/src/main/java/com/jide/ppmtool/services/ProjectService.java
@@ -1,4 +1,18 @@
 package com.jide.ppmtool.services;
 
+import com.jide.ppmtool.model.Project;
+import com.jide.ppmtool.respositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+
+    public Project saveOrUpdate(Project project){
+        return  projectRepository.save(project);
+    }
 }
