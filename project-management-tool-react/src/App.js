ed88a29c67071edc125b085ee650d596b0f3b28c
Jideofor Nwoke <njusticej@gmail.com>
2020-02-21 15:43:20 +0100

    add react

---
+++ project-management-tool-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DashBoard from './components/DashBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <DashBoard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ project-management-tool-react/src/components/DashBoard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DashBoard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>welcome to the dashboard</h1>
+            </div>
+        )
+    }
+}
