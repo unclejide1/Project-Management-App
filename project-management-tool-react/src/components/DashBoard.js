a5f3c7793c6ee5c8009d5b384787f39f00fe0a35
Jideofor Nwoke <njusticej@gmail.com>
2020-02-21 16:32:04 +0100

    add dashboard jsx to dashboard.js

--- project-management-tool-react/src/components/DashBoard.js
+++ project-management-tool-react/src/components/DashBoard.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import ProjectItem from './project/ProjectItem'
+
 
 export default class DashBoard extends Component {
     render() {
         return (
-            <div>
-                <h1>welcome to the dashboard</h1>
+            
+            <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                            Create a Project
+                        </a>
+                        <br />
+                        <hr />
+    
+                      <ProjectItem/>
+                    </div>
+                </div>
             </div>
+        </div>
         )
     }
 }
