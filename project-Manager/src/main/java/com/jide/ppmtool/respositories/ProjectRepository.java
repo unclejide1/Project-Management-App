b8ec6f363a6691699aaebef083a42cde0381dafc
Jideofor Nwoke <njusticej@gmail.com>
2020-02-21 01:26:41 +0100

    add project Respository

--- project-Manager/src/main/java/com/jide/ppmtool/respositories/ProjectRepository.java
+++ project-Manager/src/main/java/com/jide/ppmtool/respositories/ProjectRepository.java
@@ -1,4 +1,13 @@
 package com.jide.ppmtool.respositories;
 
-public interface ProjectRepository {
+
+import com.jide.ppmtool.model.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
 }
