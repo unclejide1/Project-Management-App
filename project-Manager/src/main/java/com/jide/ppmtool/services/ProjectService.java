1fc25a7bcb0b1d7ac0579099fc8c29c36a806752
Jideofor Nwoke <njusticej@gmail.com>
2020-02-21 12:13:10 +0100

    add methods to get a project, get all projects and delete a project in the project services

--- project-Manager/src/main/java/com/jide/ppmtool/services/ProjectService.java
+++ project-Manager/src/main/java/com/jide/ppmtool/services/ProjectService.java
@@ -1,5 +1,6 @@
 package com.jide.ppmtool.services;
 
+import com.jide.ppmtool.exceptions.ProjectIdException;
 import com.jide.ppmtool.model.Project;
 import com.jide.ppmtool.respositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,36 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
 
-    public Project saveOrUpdate(Project project){
-        return  projectRepository.save(project);
+    public Project saveOrUpdate(Project project) {
+
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return  projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() +"' already exists");
+        }
+
+    }
+
+    public  Project findByProjectId(String projectId){
+        Project foundProject = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(foundProject == null){
+            throw new ProjectIdException("Project with " + projectId +" does not exist");
+        }
+
+        return  foundProject;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier (String projectId){
+        Project projectToDelete = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(projectToDelete == null){
+            throw new ProjectIdException("Cannot delete Project with ProjectId '" + projectId +"'. This Project does not exist");
+        }
+        projectRepository.delete(projectToDelete);
     }
 }
