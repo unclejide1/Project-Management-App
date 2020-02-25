e60da1562ff1c8300d74051620788f2b040bede4
Jideofor Nwoke <njusticej@gmail.com>
2020-02-25 19:28:50 +0100

    add hooking up react with redux

--- project-management-tool-react/src/App.js
+++ project-management-tool-react/src/App.js
@@ -5,9 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/layout/Header';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from './components/project/AddProject'
+import {Provider} from 'react-redux';
+import store from './store'
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
     <div className="App">
       <Header />
@@ -15,6 +18,7 @@ function App() {
       <Route exact path="/addProject" component={AddProject} />
     </div>
   </Router>
+  </Provider>
   );
 }
 
