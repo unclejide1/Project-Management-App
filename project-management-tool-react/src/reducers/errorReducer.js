d2be730c5a3ee7b8f0aa8d21b4fb7466bb8e1be4
Jideofor Nwoke <njusticej@gmail.com>
2020-02-25 21:28:25 +0100

    add errorReducer to manage state if there is error

---
+++ project-management-tool-react/src/reducers/errorReducer.js
@@ -0,0 +1,13 @@
+import {GET_ERRORS} from '../actions/types'
+
+const initialState = {};
+
+export default function(state = initialState, actions){
+    switch(actions.type){
+
+        case GET_ERRORS:
+            return actions.payload;
+        default:
+            return state;
+    }
+}
