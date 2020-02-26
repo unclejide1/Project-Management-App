0c660fee885cfb6714d1d25b5496eff309f3b9a0
Jideofor Nwoke <njusticej@gmail.com>
2020-02-26 14:33:13 +0100

    finished updating projects functionality

--- project-management-tool-react/src/components/project/updateProject.js
+++ project-management-tool-react/src/components/project/updateProject.js
@@ -1,33 +1,103 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {getProject, createProject} from "../../actions/projectActions";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import classnames from "classnames";
 
  class updateProject extends Component {
+    constructor (){
+        super()
+        this.state= {
+            id:"",
+            projectName:"",
+            projectIdentifier:"",
+            projectDescription:"",
+            startDate:"",
+            endDate:""
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps){
+        const {id,
+        projectName,
+        projectIdentifier,
+        projectDescription,
+        startDate,
+        endDate} = nextProps.project;
+
+        this.setState({
+            id,
+        projectName,
+        projectIdentifier,
+        projectDescription,
+        startDate,
+        endDate
+        })
+    }
+
+    componentDidMount(){
+        const {id} = this.props.match.params;
+        this.props.getProject(id, this.props.history);
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        const updateProject = {
+            id: this.state.id,
+           projectName:this.state.projectName,
+           projectIdentifier:this.state.projectIdentifier,
+           projectDescription:this.state.projectDescription,
+           startDate:this.state.startDate,
+           endDate:this.state.endDate
+        }
+        this.props.createProject(updateProject, this.props.history)
+    }
+
     render() {
         return (
             <div className="register">
         <div className="container">
             <div className="row">
                 <div className="col-md-8 m-auto">
-                    <h5 className="display-4 text-center">Edit Project</h5>
+                    <h5 className="display-4 text-center">Update Project</h5>
                     <hr />
-                    <form>
-                        <div className="form-group">
-                            <input type="text" className="form-control form-control-lg " placeholder="Project Name" />
+                    <form onSubmit = {this.onSubmit}>
+                        <div className="form-group" >
+                            <input type="text" className="form-control form-control-lg " placeholder="Project Name" name="projectName" 
+                            value ={this.state.projectName}
+                            onChange = {this.onChange}/>
                         </div>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID"
+                            <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID" name ="projectIdentifier"
+                            value ={this.state.projectIdentifier}
+                            onChange = {this.onChange}
                                 disabled />
                         </div>
                         
                         <div className="form-group">
-                            <textarea className="form-control form-control-lg" placeholder="Project Description"></textarea>
+                            <textarea className="form-control form-control-lg" placeholder="Project Description" name = "projectDescription"
+                            value ={this.state.projectDescription}
+                            onChange = {this.onChange}></textarea>
                         </div>
                         <h6>Start Date</h6>
                         <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="start_date" />
+                            <input type="date" className="form-control form-control-lg" name="startDate" 
+                            value ={this.state.startDate}
+                            onChange = {this.onChange}/>
                         </div>
                         <h6>Estimated End Date</h6>
                         <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="end_date" />
+                            <input type="date" className="form-control form-control-lg" name="endDate"
+                            value ={this.state.endDate} 
+                            onChange = {this.onChange}/>
                         </div>
 
                         <input type="submit" className="btn btn-primary btn-block mt-4" />
@@ -40,4 +110,14 @@ import React, { Component } from 'react'
     }
 }
 
-export default updateProject
+updateProject.propTypes = {
+    getProject: PropTypes.func.isRequired,
+    createProject: PropTypes.func.isRequired,
+    project: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state =>({
+    project: state.project.project
+})
+
+export default connect(mapStateToProps, {getProject, createProject}) (updateProject);
