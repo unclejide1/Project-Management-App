968cb3d6d2a4eb1c2db55007bbd8bfe57cccfb3e
Jideofor Nwoke <njusticej@gmail.com>
2020-02-28 00:33:24 +0100

    add  ProjectTaskRepository

--- project-Manager/src/main/java/com/jide/ppmtool/services/ProjectService.java
+++ project-Manager/src/main/java/com/jide/ppmtool/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.jide.ppmtool.services;
 
 import com.jide.ppmtool.exceptions.ProjectIdException;
+import com.jide.ppmtool.model.Backlog;
 import com.jide.ppmtool.model.Project;
+import com.jide.ppmtool.respositories.BackLogRepository;
 import com.jide.ppmtool.respositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,27 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BackLogRepository backLogRepository;
+
 
     public Project saveOrUpdate(Project project) {
 
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if(project.getId() == null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+//                project.setCreatedAt(project.getCreatedAt());
+//                project.setUpdatedAt(project.getUpdatedAt());
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if(project.getId() != null){
+                project.setBacklog(backLogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
             return  projectRepository.save(project);
         } catch (Exception e) {
             throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() +"' already exists");
