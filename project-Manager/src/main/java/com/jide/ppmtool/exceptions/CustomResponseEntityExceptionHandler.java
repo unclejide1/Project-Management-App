2b6519c606e5a46833a466bb04ee9dfb3102d306
Jideofor Nwoke <njusticej@gmail.com>
2020-02-29 22:21:01 +0100

    customised CustomResponseEntityExceptionHandler to handle ProjectTask not found exception

--- project-Manager/src/main/java/com/jide/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ project-Manager/src/main/java/com/jide/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -15,7 +15,13 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
     @ExceptionHandler
     public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request){
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
-        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request){
+        ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
